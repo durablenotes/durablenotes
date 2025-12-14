2f96091756be14637ff30f3cc3424aa488990e53
syedwasiqbukhari-123 <durablenotes@gmail.com>
2025-12-14 20:38:37 +0500

    admin fix 4

--- .wrangler/tmp/dev-uaEfDC/index.js
+++ .wrangler/tmp/dev-uaEfDC/index.js
@@ -262,7 +262,7 @@ var jsonError = /* @__PURE__ */ __name(async (request, env, _ctx, middlewareCtx)
 }, "jsonError");
 var middleware_miniflare3_json_error_default = jsonError;
 
-// .wrangler/tmp/bundle-eFJZqG/middleware-insertion-facade.js
+// .wrangler/tmp/bundle-481yAb/middleware-insertion-facade.js
 var __INTERNAL_WRANGLER_MIDDLEWARE__ = [
   middleware_ensure_req_body_drained_default,
   middleware_miniflare3_json_error_default
@@ -294,7 +294,7 @@ function __facade_invoke__(request, env, ctx, dispatch, finalMiddleware) {
 }
 __name(__facade_invoke__, "__facade_invoke__");
 
-// .wrangler/tmp/bundle-eFJZqG/middleware-loader.entry.ts
+// .wrangler/tmp/bundle-481yAb/middleware-loader.entry.ts
 var __Facade_ScheduledController__ = class ___Facade_ScheduledController__ {
   constructor(scheduledTime, cron, noRetry) {
     this.scheduledTime = scheduledTime;
